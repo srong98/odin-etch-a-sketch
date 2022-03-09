d9d8c17981c6a7947477c007e9942f690480f5b4
Sam Rong <srong19@gmail.com>
2022-03-09 16:44:11 -0500

    Complete basic sketch pad

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const defaultSize = 48;
+const defaultColor = '#000000';
 let currentSize = defaultSize;
+let currentColor = defaultColor;
 
 const toggleSwitch = document.getElementById('toggle');
 const sliderGrid = document.getElementById('penslider');
+const colorChoice = document.getElementById('color-selection');
 const drawingBox = document.getElementById('drawing-box');
+const eraser = document.getElementById('eraser');
+const clearCanvas = document.getElementById('clear-canvas')
 
 toggleSwitch.addEventListener('click', toggleDarkMode);
 sliderGrid.addEventListener('input', updateCurrentSize);
 sliderGrid.addEventListener('change', gridReload);
+colorChoice.addEventListener('input', updateCurrentColor);
+eraser.addEventListener('click', useEraser);
+clearCanvas.addEventListener('click', gridReload);
 
 function toggleDarkMode() {
     const body = document.querySelector('body');
@@ -25,20 +33,41 @@ function updateCurrentSize() {
     currentSize = invertSliderGrid(sliderGrid.value);
     return currentSize;
 }
+
+function updateCurrentColor() {
+    currentColor = colorChoice.value;
+    return currentColor;
+}
+
+function useEraser() {
+    currentColor = '#FFFFFF';
+    return currentColor;
+}
+
+let mouseClick = false;
+drawingBox.addEventListener('mousedown', (e) => mouseClick = true);
+drawingBox.addEventListener('mouseup', (e) => mouseClick = false);
+
+function draw(e) {
+    if (!mouseClick) return;
+    e.target.style.backgroundColor = currentColor;
+}
+
+function createDivEach(size) {
+    for (let i = 0; i < (size ** 2); i++) {
+        const blockDiv = document.createElement('div');
+        blockDiv.className ='gridBlock';
+        blockDiv.addEventListener('mouseover', draw);
+        drawingBox.appendChild(blockDiv);
+    }
+}
+
 function gridSetup(size) {
     drawingBox.setAttribute('style', `grid-template-columns: repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`);
 }
 
 function clearGrid() {
     drawingBox.innerHTML = '';
-}
-
-function createDivEach(size) {
-    for (let i = 0; i < (size ** 2); i++) {
-        let blockDiv = document.createElement('div');
-        blockDiv.className ='gridBlocks';
-        drawingBox.appendChild(blockDiv);
-    }
 }
 
 function gridReload() {
